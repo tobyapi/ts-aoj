65af17d9e9a2600eae41b02b3d99df9d6754ce2f
tobyapi <landofscala@gmail.com>
2021-03-08 23:03:25 +0900

    Add submission api

--- src/submission.ts
+++ src/submission.ts
@@ -19,7 +19,36 @@ interface Submission {
   token: string | null,
 }
 
+interface SourceCode {
+  judgeId: number,
+  userId: string,
+  problemId: string,
+  language: string,
+  cpuTime: number,
+  memory: number,
+  submissionDate: number,
+  policy: string,
+  sourceCode: string,
+  reviewed: number
+}
+
 type Submissions = Array<Submission>
+
+export const post = async (problemId: string, language: string, sourceCode: string) => {
+  const url = `${AOJ_URL}/submissions`
+  const response = await axios.post<Submissions>(url, {
+    problemId: problemId,
+    language: language,
+    sourceCode: sourceCode,
+  })
+  return response
+}
+
+export const getRecent = async () => {
+  const url = `${AOJ_URL}/submission_records/recent`
+  const response = await axios.get<Submissions>(url)
+  return response
+}
 
 export const getByUserId = async (user_id: string, page: number, size: number) => {
   const params = `page=${page}&size=${size}`
@@ -28,8 +57,28 @@ export const getByUserId = async (user_id: string, page: number, size: number) =
   return response
 }
 
-export const getRecent = async () => {
-  const url = `${AOJ_URL}/submission_records/recent`
+export const getByProblemId = async (problem_id: string, page: number, size: number) => {
+  const params = `page=${page}&size=${size}`
+  const url = `${AOJ_URL}/submission_records/problems/${problem_id}?${params}`
   const response = await axios.get<Submissions>(url)
+  return response
+}
+
+export const getByUserIdAndProblemId = async (user_id: string, problem_id: string, page: number, size: number) => {
+  const params = `page=${page}&size=${size}`
+  const url = `${AOJ_URL}/submission_records/users/${user_id}/problems/${problem_id}?${params}`
+  const response = await axios.get<Submissions>(url)
+  return response
+}
+
+export const getSourceCode = async (judge_id: number) => {
+  const url = `${AOJ_URL}/reviews/${judge_id}`
+  const response = await axios.get<SourceCode>(url)
+  return response
+}
+
+export const getVerdict = async (judge_id: number) => {
+  const url = `${AOJ_URL}/verdicts/${judge_id}`
+  const response = await axios.get(url)
   return response
 }
--- test/submission.test.ts
+++ test/submission.test.ts
@@ -1,7 +1,13 @@
 import * as aoj from '../src/index'
 
-  test("get submissions", async () => {
-    const response = await aoj.submission.getRecent();
-    expect(response.status).toBe(200);
-  });
+test("get submissions", async () => {
+  const response = await aoj.submission.getRecent()
+  expect(response.status).toBe(200)
+});
+  
+
+test("get source code", async () => {
+  const response = await aoj.submission.getSourceCode(5263666)
+  expect(response.status).toBe(200)
+});
   
