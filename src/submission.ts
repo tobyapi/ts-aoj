bb405d1fe3ac00d61bd3109ac48e0c9504c60447
tobyapi <tobyapi@users.noreply.github.com>
2021-03-08 13:29:18 +0000

    Apply Prettier changes

--- src/submission.ts
+++ src/submission.ts
@@ -2,26 +2,30 @@ import axios from 'axios'
 import { AOJ_URL } from './constants'
 
 interface Submission {
-  judgeId: number,
-  judgeType: number,
-  userId: string,
-  problemId: string,
-  submissionDate: number,
-  language: string,
-  status: number,
-  cpuTime: number,
-  memory: number,
-  codeSize: number,
-  accuracy: string,
-  judgeDate: number,
-  score: number,
-  problemTitle: string | null,
-  token: string | null,
+  judgeId: number
+  judgeType: number
+  userId: string
+  problemId: string
+  submissionDate: number
+  language: string
+  status: number
+  cpuTime: number
+  memory: number
+  codeSize: number
+  accuracy: string
+  judgeDate: number
+  score: number
+  problemTitle: string | null
+  token: string | null
 }
 
 type Submissions = Array<Submission>
 
-export const getByUserId = async (user_id: string, page: number, size: number) => {
+export const getByUserId = async (
+  user_id: string,
+  page: number,
+  size: number,
+) => {
   const params = `page=${page}&size=${size}`
   const url = `${AOJ_URL}/submission_records/users/${user_id}?${params}`
   const response = await axios.get<Submissions>(url)
